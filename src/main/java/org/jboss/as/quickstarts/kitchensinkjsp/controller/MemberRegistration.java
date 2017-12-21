8694bd931f71266bd7d97f56636f8a1dfd251813
avatar42 avatar42_github@m42.biz <avata@Hero.localdomain>
2017-12-21 15:04:22 -0600

    debug

--- src/main/java/org/jboss/as/quickstarts/kitchensinkjsp/controller/MemberRegistration.java
+++ src/main/java/org/jboss/as/quickstarts/kitchensinkjsp/controller/MemberRegistration.java
@@ -63,7 +63,6 @@ public class MemberRegistration {
     public void register() throws Exception {
 
         try {
-
             log.info("Registering " + newMember.getName());
             em.persist(newMember);
             memberEventSrc.fire(newMember);
